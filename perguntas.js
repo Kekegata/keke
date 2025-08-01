cd3b89834b79b478041c47feb1dca4c0b60e6526
Kekegata <kemily.rodrigues.morais@escola.pr.gov.br>
2025-08-01 19:19:10 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Estetica',
+    'O que é Belo?',
+    'O belo é algo que causa prazer, admiração ou deleite, seja pela sua forma, proporções harmoniosas, beleza estética ou por um significado.'
+)
+
+criaCartao(
+    'Estetica',
+    'O que é Beleza?',
+    'Qualidade, propriedade, caráter ou virtude do que é belo; manifestação característica do belo.'
+)
+
+criaCartao(
+    'Estetica',
+    'O que é Estetica facial?',
+    'Ramo da estética que cuida da beleza e saúde da pele do rosto, utilizando diversos tratamentos e procedimentos para melhorar a aparência e corrigir imperfeições.'
+)
+
+criaCartao(
+    'Estetica',
+    'O que é Estetica corporal?',
+    'Refere-se aos cuidados com a aparência e a saúde do corpo, envolvendo procedimentos e tratamentos que visam melhorar a forma, a textura da pele, a proporção corporal e, consequentemente, a autoestima.'
+)
